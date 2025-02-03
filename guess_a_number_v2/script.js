d014f8c4d8e82782e28d4926e3bff5ddb12d90af
eljeys <eljeys1@gmail.com>
2025-02-03 11:10:57 +0100

    primary styling done (buttons)

--- guess_a_number_v2/script.js
+++ guess_a_number_v2/script.js
@@ -20,34 +20,44 @@ let numberOfGuess = 0;
 
 //Load function
 function start() {
+  startBtn.classList.add("button_hover");
   document.querySelector("input").disabled = false;
+  startBtn.classList.add("button_hover")
+  removeHover();
   tooHighBtn.disabled = true;
   tooLowBtn.disabled = true;
   correctBtn.disabled = true;
+  resetBtn.classList.add("hidden","reset_btn")
   startBtn.addEventListener("click", startGame);
 }
 
 //click events
 function startGame() {
+  startBtn.classList.remove("button_hover")
   startBtn.disabled = true;
+  startBtn.classList.add("disabled")
   document.querySelector("input").disabled = true;
-  disabledBtn();
+  removedisabledBtn();
+  hoverBtn();
   choosenNum.textContent = ` Your choosen: ${inputEl} as the number`;
   generateChoice();
 }
 
 //extra functions
-function disabledBtn() {
+function removedisabledBtn() {
   tooHighBtn.disabled = false;
   tooLowBtn.disabled = false;
   correctBtn.disabled = false;
+  tooLowBtn.classList.remove("button_hover");
+tooHighBtn.classList.remove("button_hover");
+correctBtn.classList.remove("button_hover");
 }
 
 //computers choice
 function generateChoice() {
   console.log("computeren gætter på:", compGuess);
   compGuess = Math.floor((low + high) / 2);
-  compResult.innerHTML = `Computer guess is ${compGuess}`;
+  compResult.innerHTML = `Computer guess is: ${compGuess}`;
   numberOfGuess += 1;
   console.log(numberOfGuess);
 }
@@ -70,6 +80,9 @@ function toLow() {
 
 correctBtn.addEventListener("click", () => {
  resetBtn.classList.remove("hidden")
+ resetBtn.classList.add("button_hover");
+ removeHover();
+removedisabledBtn();
  tooHighBtn.disabled = true;
  tooLowBtn.disabled = true;
  correctBtn.disabled = true;
@@ -87,6 +100,21 @@ function clear() {
   location.reload();
 }
 
-
-
-
+//hover function:
+function hoverBtn(){
+tooLowBtn.classList.add("button_hover");
+tooHighBtn.classList.add("button_hover");
+correctBtn.classList.add("button_hover");
+tooLowBtn.classList.remove("disabled");
+tooHighBtn.classList.remove("disabled");
+correctBtn.classList.remove("disabled");
+}
+//remove hover function: 
+function removeHover(){
+tooLowBtn.classList.remove("button_hover");
+tooHighBtn.classList.remove("button_hover");
+correctBtn.classList.remove("button_hover");
+tooLowBtn.classList.add("disabled");
+tooHighBtn.classList.add("disabled");
+correctBtn.classList.add("disabled");
+}
